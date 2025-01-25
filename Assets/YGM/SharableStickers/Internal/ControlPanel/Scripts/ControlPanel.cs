191a0cea16594355b53017739159173bb46db4bd
Yuta <yunaomaki708@gmail.com>
2025-01-25 15:34:11 +0900

    コントロールパネルを作成

---
+++ Assets/YGM/SharableStickers/Internal/ControlPanel/Scripts/ControlPanel.cs
@@ -0,0 +1,25 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace YGM.SharableStickers
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class ControlPanel : UdonSharpBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        #region Unity Event
+        public void OnClickAddSticker()
+        {
+
+        }
+        #endregion
+    }
+
+}
