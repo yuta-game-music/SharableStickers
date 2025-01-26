d09ab7b20da4f2c90ab2b88d1195f3b6c63840bd
Yuta <yunaomaki708@gmail.com>
2025-01-26 13:36:33 +0900

    スポーン位置をより適切な位置に移動

--- Assets/YGM/SharableStickers/Internal/ControlPanel/Scripts/ControlPanel.cs
+++ Assets/YGM/SharableStickers/Internal/ControlPanel/Scripts/ControlPanel.cs
@@ -14,8 +14,9 @@ namespace YGM.SharableStickers
         public void OnClickAddSticker()
         {
             var randomColor = Random.ColorHSV(0, 1, 0.7f, 0.7f, 0.8f, 0.8f, 0.7f, 0.7f);
-            var playerFrontPosition = LocalPlayer.GetPosition() + LocalPlayer.GetRotation() * new Vector3(0, -0.5f, 1f);
-            var playerFaceRotation = LocalPlayer.GetRotation();
+            var playerTrackingData = LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
+            var playerFrontPosition = playerTrackingData.position + playerTrackingData.rotation * new Vector3(0, -0.5f, 1f);
+            var playerFaceRotation = playerTrackingData.rotation;
             m_system.AddNewLocalSticker("", randomColor, playerFrontPosition, playerFaceRotation);
         }
 
